97554fc89a027a144d7bf8f41d4e04ed86e9f192
LoghinElisei <loghinelisei04@gmail.com>
2026-04-11 22:27:51 +0300

    add vital functionality - play, pause, open file

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,49 @@
-﻿namespace AIMediaPlayer.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
+using Avalonia.Platform.Storage;
+using System.Linq;
+
+namespace AIMediaPlayer.ViewModels
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ObservableObject
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        [ObservableProperty]
+        private Uri? _videoSource;
+
+        public event Action? RequestPlay;
+        public event Action? RequestPause;
+
+        [RelayCommand]
+        private async Task OpenFile(IStorageProvider storageProvider)
+        {
+            var result = await storageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Alege un fișier video",
+                // Create a custom filter for video files
+                FileTypeFilter = new[]
+                {
+            new FilePickerFileType("Fișiere Video")
+            {
+                Patterns = new[] { "*.mp4", "*.mkv", "*.avi", "*.mov", "*.wmv" }
+            }
+        },
+                AllowMultiple = false
+            });
+
+            var file = result.FirstOrDefault();
+            if (file != null)
+            {
+                VideoSource = file.Path;
+                RequestPlay?.Invoke();
+            }
+        }
+
+        [RelayCommand]
+        private void Play() => RequestPlay?.Invoke();
+
+        [RelayCommand]
+        private void Pause() => RequestPause?.Invoke();
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,12 +1,66 @@
+﻿using AIMediaPlayer.ViewModels;
 using Avalonia.Controls;
+using LibVLCSharp.Avalonia;
+using LibVLCSharp.Shared;
+using System;
 
-namespace AIMediaPlayer.Views
+namespace AIMediaPlayer.Views;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private LibVLC _libVLC;
+    private MediaPlayer _mediaPlayer;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        LibVLCSharp.Shared.Core.Initialize();
+
+        _libVLC = new LibVLC();
+
+        _mediaPlayer = new MediaPlayer(_libVLC);
+
+        this.Loaded += (s, e) =>
         {
-            InitializeComponent();
+            VideoPlayer.MediaPlayer = _mediaPlayer;
+        };
+
+        DataContextChanged += (s, e) =>
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.RequestPlay += () => _mediaPlayer?.Play();
+                vm.RequestPause += () => _mediaPlayer?.Pause();
+
+                vm.PropertyChanged += (sender, args) =>
+                {
+                    if (args.PropertyName == nameof(vm.VideoSource) && vm.VideoSource != null)
+                    {
+                        _mediaPlayer.Media = new Media(_libVLC, vm.VideoSource);
+                        _mediaPlayer.Play();
+                    }
+                };
+            }
+        };
+    }
+    
+
+    private void Play_OnClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        _mediaPlayer.Play();
+    }
+    protected override void OnClosed(EventArgs e)
+    {
+        _mediaPlayer?.Dispose();
+        _libVLC?.Dispose();
+        base.OnClosed(e);
+    }
+    private void Pause_OnClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (_mediaPlayer.IsPlaying)
+        {
+            _mediaPlayer.SetPause(true);
         }
     }
 }
